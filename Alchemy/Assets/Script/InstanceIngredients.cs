93ab91f41ba64692232620cf401d5a5371f48971
suittizihou <suittizihou@gmail.com>
2019-10-04 14:23:11 +0900

    QR動いたよ(一回読み取ったオブジェクトは再度読み取っても生成されない)

---
+++ Alchemy/Assets/Script/InstanceIngredients.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceIngredients : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] ingredients;
+    [SerializeField, Tooltip("生成する場所")]
+    Transform instancePoint;
+
+    List<IngredientsName> removeNames = new List<IngredientsName>();
+
+    public enum IngredientsName
+    {
+        curryRoux,
+        carrot,
+        potato
+    }
+
+    // 具材を生成
+    public void InstanceIngredient(IngredientsName ingredientsType)
+    {
+        // 同じのがあったら返す
+        if (removeNames.Contains(ingredientsType)) return;
+
+        // 追加
+        removeNames.Add(ingredientsType);
+        Instantiate(ingredients[(int)ingredientsType], instancePoint);
+    }
+}
