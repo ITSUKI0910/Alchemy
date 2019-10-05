3a146c79debc9a3a5e1d5871e7b5451efcc21201
itsuki <itsuki2525avo@gmail.com>
2019-10-05 15:14:17 +0900

    追加しなかったから無理やり押し込む

---
+++ Alchemy/Assets/Script/TestinstansObj.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestinstansObj : MonoBehaviour
+{
+    /*
+     入ってきたストリング型から
+     モデルを持ってきて
+     生成する
+
+     自動で落下して
+     鍋といい感じに当たり判定したら
+     消してLogを出力する
+     */
+
+    Dictionary<string,string> modeldate;
+
+    //モデルを生成するところ
+    bool Generate3dModel(string modelname)
+    {
+        //まず同じ名前があるか
+        if (modeldate[modelname] !=modelname) return false;
+        //あったら生成させる
+        GameObject obj = (GameObject)Resources.Load(modeldate[modelname]);
+        Instantiate(obj, Vector3.zero, new Quaternion());
+        return true;
+    }
+    void Start()
+    {
+        GameObject obj = (GameObject)Resources.Load("3dModel/curryRoux/curryRoux.obj");
+        Instantiate(obj, Vector3.zero, new Quaternion());
+    }
+
+}
